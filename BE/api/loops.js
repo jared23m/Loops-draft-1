09e8e672f588377654841d8370dbd382f758a6ee
Jared May <jared23m@gmail.com>
2024-03-18 13:35:01 -0500

    loops PUT endpoint working

--- BE/api/loops.js
+++ BE/api/loops.js
@@ -3,7 +3,9 @@ const loopsRouter = express.Router();
 const { requireUser, requireAdmin } = require("./utils");
 
 const {
-    createLoop
+    createLoop, 
+    getLoopRowById,
+    updateLoop
 } = require("../db/loops");
 
 const {
@@ -89,6 +91,173 @@ loopsRouter.post("/", requireUser, async (req, res, next) => {
     try {
       const newLoop = await createLoop(loop);
       res.send(newLoop);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  loopsRouter.post("/:loopId", requireUser, async (req, res, next) => {
+    const { id: userId } = req.user;
+    const { loopId } = req.params;
+    const { body } = req;
+
+    body.parentLoopId = loopId;
+
+    body.status = 'parent';
+
+    const keySigMatch = keySigNames.find((name) => {
+        return body.keySig == name;
+    })
+
+    if (!keySigMatch){
+        next({
+            name: "KeySigInvalid",
+            message: `A loop must have one of these key signature names:
+                    "Cmaj/Amin',  
+                    "Dbmaj/Bbmin", 
+                    "Dmaj/Bmin",
+                    "Ebmaj/Cmin", 
+                    "Emaj/C#min", 
+                    "Fmaj/Dmin", 
+                    "Gbmaj/Ebmin", 
+                    "Gmaj/Emin",
+                    "Abmaj/Fmin",
+                    "Amaj/F#min",
+                    "Bbmaj/Gmin",
+                    "Bmaj/G#min"
+                    `
+          });
+    }
+
+    if (!body.relativeChordNames || body.relativeChordNames.length == 0 || body.relativeChordNames.length > 4){
+      next({
+        name: "relativeChordArrayInvalid",
+        message: `A loop must have at least 1 and no more than 4 relative chord names.`
+      });
+    }
+
+    let chordNameInvalid;
+    let counter = 0;
+
+    while (!chordNameInvalid && counter < body.relativeChordNames.length){
+      const found = relativeChordNameOptions.find((option) =>{
+        return body.relativeChordNames[counter].toLowerCase() == option;
+      });
+
+      if (!found){
+        chordNameInvalid = true;
+      }
+
+      counter = counter + 1;
+    }
+
+    if (chordNameInvalid){
+      next({
+        name: "relativeChordNameInvalid",
+        message: `A chord name must either be flat (b in front) or neutral (neither b or # in front). It must be a roman numeral 1-7, 
+        either capital or lowercase. It can have an optional "dim" suffix.`
+      });
+    }
+
+    const currentDate = new Date();
+    body.timestamp = currentDate.toLocaleString();
+    const timestampArr = body.timestamp.split(',');
+    body.title = timestampArr[0];
+    
+    const loop = { ...body, userId };
+    try {
+        const newLoop = await createLoop(loop);
+        res.send(newLoop);
+      } catch (err) {
+        next(err);
+      }
+    });
+
+
+    loopsRouter.put("/:loopId", requireUser, async (req, res, next) => {
+      const { id: userId } = req.user;
+      const { loopId } = req.params;
+      const { body } = req;
+
+      try {
+
+          const potentialLoop = await getLoopRowById(loopId);
+
+          if (potentialLoop.userid != userId){
+            next({
+              name: "InvalidCredentials",
+              message: `Tokened user did not make this loop.`
+            });
+          }
+
+          if (potentialLoop.status == 'parent'){
+            body.status = 'parent';
+          } else if  (!(body.status == 'public' || body.status == 'private' || body.status == 'followOnly')){
+            next({
+                name: "LoopStatusInvalid",
+                message: "A loop must either be public, private, or followOnly",
+              });
+           }
+
+          const keySigMatch = keySigNames.find((name) => {
+              return body.keySig == name;
+          })
+
+          if (!keySigMatch){
+              next({
+                  name: "KeySigInvalid",
+                  message: `A loop must have one of these key signature names:
+                          "Cmaj/Amin',  
+                          "Dbmaj/Bbmin", 
+                          "Dmaj/Bmin",
+                          "Ebmaj/Cmin", 
+                          "Emaj/C#min", 
+                          "Fmaj/Dmin", 
+                          "Gbmaj/Ebmin", 
+                          "Gmaj/Emin",
+                          "Abmaj/Fmin",
+                          "Amaj/F#min",
+                          "Bbmaj/Gmin",
+                          "Bmaj/G#min"
+                          `
+                });
+          }
+
+          if ((body.relativeChordNames) && (body.relativeChordNames.length == 0 || body.relativeChordNames.length > 4)){
+            next({
+              name: "relativeChordArrayInvalid",
+              message: `A loop must have at least 1 and no more than 4 relative chord names.`
+            });
+          }
+
+          if (body.relativeChordNames){
+            let chordNameInvalid;
+            let counter = 0;
+  
+            while (!chordNameInvalid && counter < body.relativeChordNames.length){
+              const found = relativeChordNameOptions.find((option) =>{
+                return body.relativeChordNames[counter].toLowerCase() == option;
+              });
+  
+              if (!found){
+                chordNameInvalid = true;
+              }
+  
+              counter = counter + 1;
+            }
+  
+            if (chordNameInvalid){
+              next({
+                name: "relativeChordNameInvalid",
+                message: `A chord name must either be flat (b in front) or neutral (neither b or # in front). It must be a roman numeral 1-7, 
+                either capital or lowercase. It can have an optional "dim" suffix.`
+              });
+            }
+          }
+          
+          const loop = { ...body, loopId};
+          const newLoop = await updateLoop(loop);
+          res.send(newLoop);
     } catch (err) {
       next(err);
     }
--- BE/db/loops.js
+++ BE/db/loops.js
@@ -25,6 +25,7 @@ async function createLoop({
     relativeChordNames
   }) {
     try {
+      console.log("parentloopid", parentLoopId);
       const {
         rows: [loop],
       } = await client.query(
@@ -111,7 +112,154 @@ async function createLoop({
         });
         })
       )
-      return getLoopRowById(loop.id);
+      return await getLoopRowById(loop.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function updateLoop({
+    status,
+    keySig,
+    relativeChordNames,
+    loopId
+  }) {
+    try {
+
+      console.log("here");
+      console.log(status);
+      console.log(keySig);
+      const {
+        rows: [loop],
+      } = await client.query(
+        `
+        UPDATE loops
+        SET status = $2, keySig = $3
+        WHERE id = $1
+        RETURNING *;
+      `,
+        [loopId, status, keySig]
+      );
+
+      console.log(loop);
+
+      let relativeChords;
+
+      if (relativeChordNames){
+
+          await client.query(
+            `
+            DELETE FROM relative_chords
+            WHERE loopId = $1;
+            `,
+            [loopId]
+          )
+
+        relativeChords = await Promise.all(
+          relativeChordNames.map((chord, index)=>{
+                  let numerals;
+                  if (chord[0] == "b"){
+                    const numeralsArr = chord.split('');
+                    numeralsArr.splice(0, 1);
+                    numerals = numeralsArr.join('');
+                  } else {
+                    numerals = chord;
+                  }
+                  let quality;
+                  if (numerals == numerals.toUpperCase()){
+                      quality = "maj";
+                  } else if (numerals[numerals.length - 1] == 'm'){
+                      quality = "dim";
+                  } else {
+                      quality = "min";
+                  }
+  
+                  let chordName;
+                  if (quality == 'dim'){
+                      const chordNameArr = chord.split('');
+                      chordNameArr.splice(chordNameArr.length - 3, 3);
+                      const newChordName = chordNameArr.join('');
+                      chordName = newChordName;
+                  } else {
+                      chordName = chord;
+                  }
+  
+                  let relativeRootId;
+                  let counter = 0;
+                  let done = false;
+                  while (!done && (counter < relativeRootIdOptions.length)){
+                      if (chordName.toLowerCase() == relativeRootIdOptions[counter]) {
+                          relativeRootId = counter;
+                          done = true;
+                      } 
+  
+                      counter = counter + 1;
+                  }
+  
+                  return createRelativeChord({
+                      loopId: loop.id, 
+                      relativeRootId, 
+                      quality, 
+                      name: chord, 
+                      position: index
+                  });
+          })
+        )
+      }
+    
+
+      let keySigIndex;
+
+      console.log("keysig",loop.keysig);
+
+        keySigNames.forEach((option, index) => {
+          if  (loop.keysig == option){
+            keySigIndex = index;
+          }
+        })
+
+        console.log("keysigindex", keySigIndex);
+
+      await client.query(
+        `
+        DELETE FROM absolute_chords
+        WHERE loopId = $1;
+        `,
+        [loopId]
+      );
+
+
+        const {rows: relativeChordsGet} = await client.query(`
+        SELECT *
+        FROM relative_chords
+        WHERE loopId = $1
+        `,
+        [loopId])
+
+      console.log("relativeChords", relativeChordsGet);
+
+      const absoluteChords = await Promise.all(
+        relativeChordsGet.map((chord) => {
+          let absoluteRootId = chord.relativerootid + keySigIndex;
+          while (absoluteRootId >= 12){
+            absoluteRootId = absoluteRootId - 12;
+          }
+          const name = `${rootShiftArr[absoluteRootId]}${chord.quality}`;
+          return createAbsoluteChord({
+            loopId: loop.id, 
+            absoluteRootId, 
+            quality: chord.quality,
+            name,
+            position: chord.position
+        });
+        })
+      )
+
+      console.log("absolute", absoluteChords);
+
+      const loopRow = await getLoopRowById(loop.id);
+      console.log("looprow", loopRow);
+      return await getLoopRowById(loop.id);
     } catch (error) {
       throw error;
     }
@@ -144,5 +292,6 @@ async function createLoop({
   }
   module.exports = {
     createLoop,
+    updateLoop,
     getLoopRowById
   }
