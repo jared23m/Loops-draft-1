0e2c0dd09278e4fb465d7892562bb2e9478b7151
Jared May <jared23m@gmail.com>
2024-04-17 12:30:26 -0500

    ux for access finished, besides search feature

--- FE/loops-draft-1/src/components/Access.jsx
+++ FE/loops-draft-1/src/components/Access.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { fetchAllUsersGet, fetchAccessGet } from "../api";
+import { useParams, useNavigate } from "react-router-dom";
+import { fetchAllUsersGet, fetchAccessGet, fetchAccessPost } from "../api";
 import { useState, useEffect } from "react";
 
 export default function Access(props){
@@ -10,6 +10,7 @@ export default function Access(props){
     const [accessList, setAccessList] = useState([]);
     const [fetched, setFetched] = useState(0);
     const {loopId} = useParams();
+    const navigate = useNavigate();
 
     useEffect(()=>{
         async function allUsersGet(token){
@@ -34,6 +35,10 @@ export default function Access(props){
                 setError(potentialAccessGet);
             } else if (potentialAccessGet){
                 let currentAllUsers = allUsers;
+                const self = currentAllUsers.findIndex((user)=>{
+                    return user.id == props.accountId;
+                })
+                currentAllUsers.splice(self, 1);
                 let potentialAccessList = [];
                 currentAllUsers.forEach((user, index)=>{
                     const found = potentialAccessGet.find((id) =>{
@@ -42,7 +47,7 @@ export default function Access(props){
 
                     if (found){
                         const splicedIndex = currentAllUsers.splice(index, 1);
-                        potentialAccessList.push(splicedIndex);
+                        potentialAccessList.push(splicedIndex[0]);
                     }
                 })
                 setAllUsers(currentAllUsers);
@@ -99,6 +104,21 @@ export default function Access(props){
         setAccessList([...currentAccessList]);
     }
 
+    async function handleAccessSubmit(){
+        setError({message: "Loading... This may take a few minutes if the server hasn't been used recently."});
+        const userArr = accessList.map((user)=>{
+            return user.id;
+        })
+        const potentialSubmit = await fetchAccessPost(props.token, loopId, userArr);
+        if (!potentialSubmit){
+            setError({message: "Failed to fetch."});
+        } else if (potentialSubmit.message && potentialSubmit.message == "Access granted to users.") {
+            navigate(`/users/${props.accountId}`);
+        } else {
+            setError(potentialSubmit);
+        }
+    }
+
     return (
         <div className='accessMaster'>
             <p className='accessTitle'>Allow Access</p>
@@ -106,6 +126,7 @@ export default function Access(props){
             <p className='errorMessage'>{error.message}</p>
         :
             <>
+            <button onClick={handleAccessSubmit}>Submit Changes</button>
             <p>Allowing Access For:</p>
             {accessList.map((user)=>{
                 return (
